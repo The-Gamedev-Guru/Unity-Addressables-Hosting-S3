20dab1bd8f362fe39f8c9fec362963641b5055cf
rubentorresbonet <cofruben@gmail.com>
2019-10-15 00:09:45 +0200

    Initial commit

---
+++ Assets/Scripts/SkyboxLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace TheGamedevGuru
+{
+    public class SkyboxLoader : MonoBehaviour
+    {
+        [SerializeField] private List<AssetReference> _skyboxMaterials = null;
+        
+        IEnumerator Start()
+        {
+            var wait = new WaitForSeconds(3f);
+            var currentIndex = 0;
+            while (true)
+            {
+                var opHandle = _skyboxMaterials[currentIndex++ % _skyboxMaterials.Count].LoadAssetAsync<Material>();
+                yield return opHandle;
+                RenderSettings.skybox = opHandle.Result;
+                yield return wait;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# The Guru's tutorial on **Unity Addressables Hosting with S3**
+
+This project will, along the article on [Addressabkes](https://thegamedev.guru/unity-addressables/hosting-with-amazon-s3/), help you getting familiarized with the hosting possibilities of the addressables system in no time.
+
+For information regarding this repository, read the article.
